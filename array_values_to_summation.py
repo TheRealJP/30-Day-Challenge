0fae301d977e4a5383d420254f22d94f813f8def
Juan Ortiz <jportiz32@gmail.com>
2019-02-20 00:17:49 -0500

    Rename day-02 to array_values_to_summation

--- array_values_to_summation.py
+++ array_values_to_summation.py
@@ -1,5 +1,4 @@
-# ==== 30-DAY CODING CHALLENGE: DAY 2 ====
-#
+# ========================================
 # Problem:
 # Given an array of integers, return a new array such that each element at index i of the new # array is the product of all the numbers in the original array except the one at i.
 #
