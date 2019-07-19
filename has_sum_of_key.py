047d78f86f27b0a219ba56d690b8baeedcb6f5e2
Juan Ortiz <jportiz32@gmail.com>
2019-07-19 13:15:01 -0400

    Implement better solution for pair with sum

--- has_sum_of_key.py
+++ has_sum_of_key.py
@@ -4,24 +4,38 @@
 # return whether any two numbers from the list add up to k.
 # For example, given [10, 15, 3, 7] and k of 17,
 # return true since 10 + 7 is 17.
+# Assume the list is unordered.
 #
-# Solution written by: Juan Ortiz
-# Time complexity for solution: O(n^2)
+# Solution(s) written by: Juan Ortiz
 # ========================================
 
-values = [10, 15, 3, 7]
-k = 17
+values = [10, 7, 15, 3, 21, 8, 45]
+expected_sum = 24
 
-
-def has_sum_of_key(array, key):
-    ''' returns True if there exists a pair of values within the array that summate to the argued key.'''
+# naive solution : time complexity of O(n^2)
+def contains_pair_with_sum_naive(array, expected_sum) -> bool:
     for x in array:  # Loop throw each value
         for y in array[1:]:    # check value against all other values
-            if x + y == key:
-                print(x, "+", y, "=", sum)
+            if x + y == expected_sum:
                 return True
-    print("No pairs equal to", key)
     return False
 
+# better soution: time complexity of O(nlog(n))
+def contains_pair_with_sum_better(array, expected_sum) -> bool:
+    array.sort()
 
-print(has_sum_of_key(values, k))
+    first_half = array[:len(array)//2]
+    second_half = array[(len(array)//2):]
+    for x in first_half:
+        for y in second_half:
+            sum = x + y
+            if sum > expected_sum:
+                y -= 1
+            elif sum < expected_sum:
+                x += 1
+            elif sum == expected_sum:
+                return True
+    return False
+
+print(contains_pair_with_sum_naive(values, expected_sum))
+print(contains_pair_with_sum_better(values, expected_sum))
